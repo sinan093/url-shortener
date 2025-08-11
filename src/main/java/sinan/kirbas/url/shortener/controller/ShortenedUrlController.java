1dba5b93a1e438a715c906694860e1a949876593
sinan.kirbas <sinankirbas93@web.de>
2025-08-11 10:03:37 +0200

    refactor ResponseEntity to RedirectView

--- src/main/java/sinan/kirbas/url/shortener/controller/ShortenedUrlController.java
+++ src/main/java/sinan/kirbas/url/shortener/controller/ShortenedUrlController.java
@@ -1,14 +1,10 @@
 package sinan.kirbas.url.shortener.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 import sinan.kirbas.url.shortener.model.ShortenedUrlDto;
 import sinan.kirbas.url.shortener.service.ShortenedUrlService;
-
-import java.net.URI;
 
 
 @RestController
@@ -18,10 +14,8 @@ public class ShortenedUrlController {
     private final ShortenedUrlService shortenedUrlService;
 
     @GetMapping(value = "/get/{id}")
-    public ResponseEntity<Void> getUrlById(@PathVariable String id) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(URI.create(shortenedUrlService.getShortenedUrl(id)));
-        return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
+    public RedirectView getUrlById(@PathVariable String id) {
+        return new RedirectView(shortenedUrlService.getShortenedUrl(id));
     }
 
     @PostMapping("/create")
